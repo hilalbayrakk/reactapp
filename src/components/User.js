082f7766f595060381dea8bd73a6158733d0f445
Hilal BAYRAK <hilalbayrakx54@gmail.com>
2022-10-20 18:03:32 +0300

    state added

--- src/components/User.js
+++ src/components/User.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class User extends Component {
+    /*state = {
+        isVisible : false
+    }*/
     static defaultProps = {
         name: "Bilgi Yok",
         department: "Bilgi Yok",
         salary: "Bilgi Yok"
     }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isVisible: false
+        }
+    }
     render() {
 
         //Destructing
         const { name, department, salary } = this.props;
+        const { isVisible } = this.state;
         return (
-            <div>
-                <ul>
-                    <li>İsim : {name} </li>
-                    <li>Departman : {department}</li>
-                    <li>Maaş : {salary}</li>
-                </ul>
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between">
+                        <h4 className="d-inline">{name}</h4>
+                        <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
+
+                    </div>
+                    {
+                        isVisible ? <div className="card-body">
+
+                            <p className="card-text">Maaş: {salary}</p>
+                            <p className="card-text">Departman: {department}</p>
+
+                        </div> : null
+                    }
+
+                </div>
             </div>
         )
     }
